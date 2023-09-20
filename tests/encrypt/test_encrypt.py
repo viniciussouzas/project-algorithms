4413ac619c29e84ad7505f3f6fb5b711d48a7cea
Vinícius Souza <tradutorvinicius@gmail.com>
2023-09-20 15:17:46 -0300

    fix: desafio 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -9,3 +9,4 @@ def test_encrypt_message():
         encrypt_message(message=2, key=1)
     
     assert encrypt_message("borboleta", 1) == "b_atelobro"
+    assert encrypt_message("borboleta", 10) == "atelobrob"
