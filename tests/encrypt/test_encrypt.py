fcbd3016512f1fca39f22b3c0a88fc308c1f8ccc
Vinícius Souza <tradutorvinicius@gmail.com>
2023-09-20 15:16:06 -0300

    fix lint

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,6 +1,7 @@
 from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
+
 def test_encrypt_message():
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message(message="borboleta", key="xablau")
