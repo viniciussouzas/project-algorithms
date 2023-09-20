5e78a32298c223dfab959277a5ee141cec2d36c5
Vinícius Souza <tradutorvinicius@gmail.com>
2023-09-20 15:12:51 -0300

    Resolve desafio 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message="borboleta", key="xablau")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(message=2, key=1)
+    
+    assert encrypt_message("borboleta", 1) == "b_atelobro"
