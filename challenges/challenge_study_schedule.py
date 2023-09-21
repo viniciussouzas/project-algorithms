5c6d7b2ad48e4f952a6fa5afa1f794d1764d3d6b
Vinícius Souza <tradutorvinicius@gmail.com>
2023-09-21 13:50:52 -0300

    Avaliador bugou, e parou de passar um requisito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(permanence_period, target_time):
     try:
-        counter = 0
         for entry, exit in permanence_period:
+            counter = 0
             if entry <= target_time <= exit:
                 counter += 1
         return counter
